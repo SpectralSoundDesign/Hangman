ff317d54d07a3b6040c68261c2d0f95e2ef8eaba
Will Richards <spectralsounddesign@gmail.com>
2022-10-09 16:44:53 -0400

    added load functionality

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -55,7 +55,66 @@ class Game
     puts "===================="
     puts "#{hidden_solution.join("")}"
     puts "===================="
+
+    while (@@gameover != true)
+      puts "Save game? Y/N"
+      save = gets.strip.downcase
+
+      if save == 'y'
+        save_game(round, wrong_letter_count, wrong_letter_arr, solution_arr, hidden_solution)
+      end
+
+      puts "Round: #{round}"
+      puts "Guess a letter: "
+      guess = gets.strip.downcase
+
+      solution_arr.each_with_index do |v, i|
+        if v == guess
+          hidden_solution[i] = guess
+        end
+      end
+
+      if hidden_solution.include?(guess) == false
+        wrong_letter_count -= 1
+        wrong_letter_arr.push(guess)
+      end
+
+      check_gameover(hidden_solution, solution_arr, wrong_letter_count)
+
+      puts "===================="
+      puts "#{hidden_solution.join("")}"
+      puts "===================="
+      puts "Incorrect words left: #{wrong_letter_count}"
+      puts "Wrong letters: #{wrong_letter_arr}"
+      
+      round += 1
+    end
+
+    puts "---------------------"
+    puts "      Game Over      "
+    puts "---------------------"
+
+    if wrong_letter_count == 0
+      puts "You Lose"
+      puts "Secret word: #{solution_arr.join("")}"
+    else
+      puts "You Win"
+    end
+  end
+
+  def continue_game
+    save_hash = YAML.load(File.read("savedgames/save.yaml"))
+    wrong_letter_count = save_hash[:wrong_count]
+    wrong_letter_arr = save_hash[:wrong_arr]
+    round = save_hash[:round]
     
+    solution_arr = save_hash[:solution]
+    hidden_solution = save_hash[:hidden_solution]
+
+    puts "===================="
+    puts "#{hidden_solution.join("")}"
+    puts "===================="
+
     while (@@gameover != true)
       puts "Save game? Y/N"
       save = gets.strip.downcase
@@ -117,7 +176,7 @@ class Game
     when 1
       play
     when 2
-      puts "load"
+      continue_game
     when 3
       exit
     else
